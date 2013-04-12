69f5446419c7ed098d4615a00d139c76c5b32f5b
Jiri Marsik <jiri.marsik89@gmail.com>
2013-04-13 01:10:41 +0200

    Making the mouse wheel scroll faster.

--- config/bindings.el
+++ config/bindings.el
@@ -1,5 +1,20 @@
 ;; Place your bindings here.
-
+hk
 ;; For example:
 ;;(define-key global-map (kbd "C-+") 'text-scale-increase)
 ;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+(defun wheel-scroll-down ()
+  "Scrolls the text down a few lines (up in Emacs-speak). For use
+  with the mouse wheel."
+  (interactive)
+  (scroll-up 4))
+
+(defun wheel-scroll-up ()
+  "Scrolls the text up a few lines (up in Emacs-speak). For use
+  with the mouse wheel."
+  (interactive)
+  (scroll-down 4))
+
+(define-key global-map [mouse-5] 'wheel-scroll-down)
+(define-key global-map [mouse-4] 'wheel-scroll-up)
